31fabf7d49a0691e820c70839cca4c1087e816b3
elevenia <elevenia@XLP-FANDHIs-MacBook-Pro.local>
2016-03-18 11:38:57 +0700

    add new file (service)

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IW" },
+    { "id": 19, "name": "Mrs. ds89y" },
+    { "id": 20, "name": "ncxhi" }
+];
