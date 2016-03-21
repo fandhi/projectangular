ed45c0120c5b84961ecb87da8479f4306aef0e71
elevenia <elevenia@XLP-FANDHIs-MacBook-Pro.local>
2016-03-21 19:32:47 +0700

    edit for heroes component

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,3 @@
 import {bootstrap}    from 'angular2/platform/browser'
-import {HeroesComponent} from './heroes.component'
-bootstrap(HeroesComponent);
+import {AppComponent} from './app.component'
+bootstrap(AppComponent);
